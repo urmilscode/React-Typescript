071d96390a860e57ffbb9ca6b4836e07cafb6dbd
Urmil Shah <ucodes28@gmail.com>
2024-06-23 17:38:36 +0530

    typescript file update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 var awesome = "Its going to be awesome yeah!!";
 var footballer = "Zidane";
 footballer = "Romario";
+footballer.toLowerCase();
+var player = "Zidane";
+var jersey = 5;
+var stillPlaying = false;
 document.getElementsByTagName("p")[0].innerText = awesome;
 document.getElementsByTagName("p")[1].innerText = footballer;
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(3, 4);
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 const awesome = "Its going to be awesome yeah!!"; 
 let footballer: string = "Zidane";
 footballer = "Romario";
+footballer.toLowerCase()
+let player: string = "Zidane";
+let jersey: number = 5; 
+let stillPlaying: boolean = false;
+
 document.getElementsByTagName("p")[0].innerText = awesome;
 document.getElementsByTagName("p")[1].innerText = footballer;
+
+function sum (a, b) {
+    return a + b; 
+}
+
+let result = sum(3,4);
+console.log(result);

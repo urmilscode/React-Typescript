1ec3cf261fc96ce555f092dc8712a872d4939e5d
Urmil Shah <ucodes28@gmail.com>
2024-06-23 16:39:43 +0530

    initial-commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var awesome = "Its going to be awesome yeah!!";
+var footballer = "Zidane";
+footballer = "Romario";
+document.getElementsByTagName("p")[0].innerText = awesome;
+document.getElementsByTagName("p")[1].innerText = footballer;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const awesome = "Its going to be awesome yeah!!"; 
+let footballer: string = "Zidane";
+footballer = "Romario";
+document.getElementsByTagName("p")[0].innerText = awesome;
+document.getElementsByTagName("p")[1].innerText = footballer;
